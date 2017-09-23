6478dad49eb32afb2b2717e92700094f1caba17a
rika <rima361@naver.com>
2017-09-24 03:50:29 +0900

    navigate next page

--- crawler/crawler.rb
+++ crawler/crawler.rb
@@ -1,9 +1,4 @@
 
-# 1. input 에 따른 주소가져오기
-# 2. input 결과 창에서 블로그 버튼 눌렀을때로 이동
-# 3. 블로그 결과에서 title, 소제 가져오기
-# 4. 다음 페이지로 넘어가기
-
 # daum form_name : daumSearch
 	 # input : q
 # name form_name : sfrom
@@ -13,7 +8,7 @@ require 'rubygems'
 require 'mechanize'
 require 'rest-client'
 
-# 1번 시작
+# main가져오기
 agent = Mechanize.new
 page = agent.get "http://naver.com"
 search_form = page.form_with :name => "sform"
@@ -21,41 +16,37 @@ search_form.field_with(:name=>"query").value = "유모차"
 search_results = agent.submit search_form
 main_uri = search_results.uri
 # puts main_uri
-# 1번 완료
 
-
-# 2번 시작
-agent = Mechanize.new
-blog_uri = agent.get(main_uri)
+# 메인에서 블로그 이동
 page = agent.page.link_with(:text => '블로그').click
-# puts page.uri
-# 2번 완료
 
-# 3번 시작
-html = agent.get(page.uri).body
-html_doc = Nokogiri::HTML(html)
-blog_section = html_doc.css('ul#elThumbnailResultArea.type01')
-blog_head = blog_section.css('dt').css('a')
-blog_content = blog_section.css('dd.sh_blog_passage')
-blog_date = blog_section.css('dd.txt_inline')
+# 페이지를 5번째 페이지까지
+for i in 2..5
 
-# title 10개를 차례대로 뽑기
-blog_head.each_with_index do |v, i|
-	puts i
-	puts v.attr('title')
+	html = agent.get(page.uri).body
+	html_doc = Nokogiri::HTML(html)
+	blog_section = html_doc.css('ul#elThumbnailResultArea.type01')
+	blog_head = blog_section.css('dt').css('a')
+	blog_mini_content = blog_section.css('dd.sh_blog_passage')
+	blog_date = blog_section.css('dd.txt_inline')
+
+	# title 10개를 차례대로 뽑기
+	blog_head.each_with_index do |v, i|
+		puts i
+		puts v.attr('title')
+	end
+
+	# 소주제 10개를 차례대로 뽑기
+	blog_mini_content.each_with_index do |v, i|
+		puts i
+		puts v.text
+	end
+
+	# 이것은 날짜 뽑기 미완성 (본문에서도 가능하므로 일단 보류)
+	# blog_date.each_with_index do |v, i|
+	# 	puts i
+	# 	puts v.first
+	# end
+
+	page = agent.page.link_with(:text => '다음페이지').click
 end
-
-# 소주제 10개를 차례대로 뽑기
-blog_content.each_with_index do |v, i|
-	puts i
-	puts v.text
-end
-
-blog_date.each_with_index do |v, i|
-	puts i
-	puts v.first
-end
-# 3번 완료
-
-# 4번 시작
-
---
+++ crawler/page_test.rb
@@ -0,0 +1,62 @@
+
+# 1. input 에 따른 주소가져오기
+# 2. input 결과 창에서 블로그 버튼 눌렀을때로 이동
+# 3. 블로그 결과에서 title, 소제 가져오기
+# 4. 다음 페이지로 넘어가기
+
+# daum form_name : daumSearch
+	 # input : q
+# name form_name : sfrom
+	 # input : query 
+
+require 'rubygems'
+require 'mechanize'
+require 'rest-client'
+
+# 1번 시작
+agent = Mechanize.new
+page = agent.get "http://naver.com"
+search_form = page.form_with :name => "sform"
+search_form.field_with(:name=>"query").value = "유모차"
+search_results = agent.submit search_form
+main_uri = search_results.uri
+# puts main_uri
+# 1번 완료
+
+
+# 2번 시작
+page = agent.page.link_with(:text => '블로그').click
+# puts page.uri
+# 2번 완료
+
+# 3번 시작
+html = agent.get(page.uri).body
+html_doc = Nokogiri::HTML(html)
+blog_section = html_doc.css('ul#elThumbnailResultArea.type01')
+blog_head = blog_section.css('dt').css('a')
+blog_content = blog_section.css('dd.sh_blog_passage')
+blog_date = blog_section.css('dd.txt_inline')
+
+# title 10개를 차례대로 뽑기
+blog_head.each_with_index do |v, i|
+	puts i
+	puts v.attr('title')
+end
+
+# 소주제 10개를 차례대로 뽑기
+blog_content.each_with_index do |v, i|
+	puts i
+	puts v.text
+end
+
+blog_date.each_with_index do |v, i|
+	puts i
+	puts v.first
+end
+# 3번 완료
+
+# 4번 시작
+for i in 2..5
+    page = agent.page.link_with(:text => '다음페이지').click
+	puts page.uri
+end
