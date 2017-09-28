7c4c2ebd41ba3cc3bc92ebe1a40f543c147740c9
rika <rima361@naver.com>
2017-09-28 22:06:41 +0900

    naver_crawler_to_class

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,5 +1,43 @@
 class MeetingsController < ApplicationController
   before_action :set_meeting, only: [:show, :edit, :update, :destroy]
+  
+  def get_location_info
+    Meeting.new
+    
+    require 'rubygems'
+    require 'mechanize'
+    require 'rest-client'
+
+    search_arr = ['축제', '베이비페어']
+
+    search_arr.each do | search |
+      agent = Mechanize.new
+      page = agent.get "http://naver.com"
+      search_form = page.form_with :name => "sform"
+      search_form.field_with(:name=>"query").value = search
+      search_results = agent.submit search_form
+      main_uri = search_results.uri
+
+      html = agent.get(main_uri).body
+      html_doc = Nokogiri::HTML(html)
+      
+      html_doc.css('span.tit_box').css('span.date').each do |t|
+          puts t.text.slice!(0...10)
+        end 
+
+        if search.eql?('축제')
+          html_doc.css('h5').each do |t|
+            puts t.text
+          end
+      else
+        html_doc.css('h6').each do |t|
+            puts t.text
+          end
+      end
+    end
+
+  end     
+end
 
   # GET /meetings
   # GET /meetings.json
