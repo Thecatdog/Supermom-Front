0eb53fd1bbd5a4a5239158d4f8cdb0dc48ac5a9e
NohYujeong <shdbwjd0326@naver.com>
2017-09-30 03:54:35 +0000

    category_modify

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!, :except =>[:index]
     def index1
-      @categories = params[:choose_categories]
+      if params[:choose_categories].nil?
+        @categories = ""
+      else
+        @categories = params[:choose_categories]
+      end
       @categories_array = ["건강","교육","도서","생활용품","장난감","음식","여행","패션"]
     end
     
