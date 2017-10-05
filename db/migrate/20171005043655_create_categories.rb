9a8f11953b827df34af5539bd19b746da5dfe719
NohYujeong <shdbwjd0326@naver.com>
2017-10-05 05:18:24 +0000

    db_update_2

--- db/migrate/20171005043655_create_categories.rb
+++ db/migrate/20171005043655_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-      t.string :keword
+      t.string :keyword
       t.integer :crawler_id
       t.timestamps null: false
     end
